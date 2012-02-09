a97062294ac80b655f89c36c752c2632860ef463
Kasper Hartwich <kasper@hartwich.net>
2012-02-09 23:26:23 +0100

    Revert "Added support for applying templates."
    This reverts commit b8fff5b6a3d9ce835abc64754481e5eae6b4609d.

--- example.php
+++ example.php
@@ -31,9 +31,6 @@ $dns->updateRecord('test123.dk', $record['recordid'], 'A', 'www.test123.dk', '12
 //Delete record
 $dns->deleteRecord($domainid, $record['recordid'], 'A');
 
-//Use template 'gmail' to add mxrecords for use with Google Apps
-$dns->applyTemplate('test123.dk', 'gmail');
-
 //Get text response from GratisDNS. (Often empty)
 echo $dns->getResponse();
 
